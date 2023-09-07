35c180b1e138d7355aa89316966d2366df124a7a
pablosaya1 <144183138+pablosaya1@users.noreply.github.com>
2023-09-07 15:38:26 -0300

    2da actualización

--- script1.js
+++ script1.js
@@ -9,3 +9,23 @@ menuToggle.addEventListener('change', function() {
     menu.style.display = 'none';
   }
 });
+
+let slideIndex = 0;
+showSlide(slideIndex);
+
+function changeSlide(n) {
+  showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+  const slides = document.getElementsByClassName("slideshow")[0].getElementsByTagName("img");
+  if (n >= slides.length) {
+    slideIndex = 0;
+  } else if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex].style.display = "block";
+}
